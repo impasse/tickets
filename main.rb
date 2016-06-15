8d0b378f5767eb60a9d882b5c5070497f6bd1178
lingmm <450044101@qq.com>
2016-06-15 16:40:13 +0800

    fix intent

--- main.rb
+++ main.rb
@@ -1,57 +1,57 @@
 #!/usr/bin/ruby
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 require 'optparse'
 require_relative './table'
 
 module Train
-	def self.doc
-		<<-DOC.gsub(/^\s+\|?/, '')
-		Train tickets query via cli.
-		Usage:
-		#{__FILE__} [options] [update] <from> <to> [<date>]
-		Arguments:
-		update           更新数据
-		from             出发站
-		to               到达站
-		date             查询日期<YYYY-MM-DD>,默认当日
-		Options: #{yield}
-		DOC
-	end
+  def self.doc
+    <<-DOC.gsub(/^\s+\|?/, '')
+    Train tickets query via cli.
+    Usage:
+    #{__FILE__} [options] [update] <from> <to> [<date>]
+    Arguments:
+    update           更新数据
+    from             出发站
+    to               到达站
+    date             查询日期<YYYY-MM-DD>,默认当日
+    Options: #{yield}
+    DOC
+  end
 
 
-	options = ''
+  options = ''
 
-	parser = OptionParser.new do |opts|
-		opts.banner = ''
-		opts.on('-h','--help','显示帮助菜单'){ puts doc(&opts.public_method(:help)) }
-		opts.on('-d','动车'){ options << 'D' }
-		opts.on('-g','高铁'){ options << 'G' }
-		opts.on('-k','快速'){ options << 'K' }
-		opts.on('-t','特快'){ options << 'T' }
-		opts.on('-z','直达'){ options << 'Z' }
-	end
-	unless ARGV.any?
-		parser.parse(['-h'])
-	else
-		parser.parse!
-		if ARGV.length < 2
-			if ARGV[0] == 'update'
-				load './update.rb'
-			else
-				puts "至少需要2个参数，仅输入了#{ARGV.length}个"
-			end
-		else
-			if /^linux/i =~ RbConfig::CONFIG['host_os']
-				ARGV.map! do |i|
-					i.encoding == Encoding::ASCII_8BIT ? i.dup.force_encoding('utf-8') : i
-				end
-			else
-				ARGV.map! do |i|
-					i.dup.encode! 'utf-8'
-				end
-			end
-			puts TerminalTable.new(options,*ARGV)
-		end
-	end
+  parser = OptionParser.new do |opts|
+    opts.banner = ''
+    opts.on('-h','--help','显示帮助菜单'){ puts doc(&opts.public_method(:help)) }
+    opts.on('-d','动车'){ options << 'D' }
+    opts.on('-g','高铁'){ options << 'G' }
+    opts.on('-k','快速'){ options << 'K' }
+    opts.on('-t','特快'){ options << 'T' }
+    opts.on('-z','直达'){ options << 'Z' }
+  end
+  unless ARGV.any?
+    parser.parse(['-h'])
+  else
+    parser.parse!
+    if ARGV.length < 2
+      if ARGV[0] == 'update'
+        load './update.rb'
+      else
+        puts "至少需要2个参数，仅输入了#{ARGV.length}个"
+      end
+    else
+      if /^linux/i =~ RbConfig::CONFIG['host_os']
+        ARGV.map! do |i|
+          i.encoding == Encoding::ASCII_8BIT ? i.dup.force_encoding('utf-8') : i
+        end
+      else
+        ARGV.map! do |i|
+          i.dup.encode! 'utf-8'
+        end
+      end
+      puts TerminalTable.new(options,*ARGV)
+    end
+  end
 end
--- update.rb
+++ update.rb
@@ -1,22 +1,27 @@
 #!/usr/bin/ruby
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 require 'open-uri'
 require 'openssl'
 
 begin
-	open('https://kyfw.12306.cn/otn/resources/js/framework/station_name.js',{ssl_verify_mode:OpenSSL::SSL::VERIFY_NONE}) do |f|
-		datas = f.read.match(/'(.+)'/)[1].split('|')
-		out = ''
-		for i in 0..(datas.length/6)
-			s = 5*i+1
-			out << "#{datas[s]} #{datas[s+1]}\n"
-		end
-		open('./stations.dat','w:UTF-8') do |w|
-			w.write(out)
-			puts "更新完成"
-		end
-	end
+  open('https://kyfw.12306.cn/otn/resources/js/framework/station_name.js',{ssl_verify_mode:OpenSSL::SSL::VERIFY_NONE}) do |f|
+    datas = f.read.match(/'(.+)'/)[1].split('|')
+    out = ''
+    i = 0
+    loop do
+      s = 5*i+1
+      i+=1
+      if s>=datas.length
+        break
+      end
+      out << "#{datas[s]} #{datas[s+1]}\n"
+    end
+    open('./stations.dat','w:UTF-8') do |w|
+      w.write(out)
+      puts "更新完成"
+    end
+  end
 rescue =>e
-	puts "更新错误:#{e.message}"
+  puts "更新错误:#{e.message}"
 end
